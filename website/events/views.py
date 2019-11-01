d1b109b8076cba2698758746b21fd3d7e8d0742c
Shrey Goel <45508538+shreygoel7@users.noreply.github.com>
2019-11-01 15:20:49 +0530

    Added cards to show events

--- website/events/views.py
+++ website/events/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
+from .models import Event
+import datetime
 
 # Create your views here.
 
 def past(request):
-    return render(request, 'events/past.html')
+    events = Event.objects
+    return render(request, 'events/past.html', {'events':events})
 
 def present(request):
-    return render(request, 'events/present.html')
+    events = Event.objects
+    for event in events.all() :
+        d1 = datetime.date.today()
+        d2 = event.event_date
+        if d2>d1:
+            event.event_time = 'f'
+            event.save()
+        elif d1>d2:
+            event.event_time = 'p'
+            event.save()
+        else:
+            event.event_time = 'n'
+            event.save()
+    return render(request, 'events/present.html', {'events':events})
 
 def future(request):
-    return render(request, 'events/future.html')
+    events = Event.objects
+    return render(request, 'events/future.html', {'events':events})
